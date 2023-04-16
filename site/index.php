83692acb1c1648f1092e99e0437fada891a3eba8
Saabbass <178414@novacollege.nl>
2023-04-16 17:06:21 +0200

    more responsive

--- site/index.php
+++ site/index.php
@@ -19,16 +19,21 @@
             <section class="white_block">
                 <div class="tekst_titel">
                     <h2>
-
+                        Lorem ipsum
                     </h2>
                 </div>
                 <div class="tekst_container">
-                    <div class="tekst">
-
+                    <div class="image_container">
+                        <img src="img/imageGimp.png" alt="">
                     </div>
 
-                    <div class="image_container">
-                        <img src="" alt="">
+                    <div class="tekst">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut justo eros, venenatis in lectus non, aliquam fringilla ante. Aenean ligula urna, venenatis id risus nec, lacinia vehicula lorem. Nullam in hendrerit urna. Nulla facilisi. Mauris libero nisl, mattis aliquet viverra eu, maximus efficitur neque. In hac habitasse platea dictumst. Suspendisse potenti. Cras et ligula nec turpis elementum lacinia non tempor mauris. Aliquam faucibus est non mi auctor, non venenatis diam tristique. Proin tincidunt leo interdum justo malesuada dictum eget eu magna. In id augue ac eros sagittis dignissim in ut est. Nunc ipsum tellus, lobortis ut magna et, sollicitudin commodo lorem. In pretium mollis aliquet. Nulla finibus est velit, eget lobortis tortor tincidunt nec. Integer feugiat lobortis justo in pretium. Donec sollicitudin nisl viverra mollis mattis.
+                        </p>
+                        <p>
+                            Maecenas ultricies odio nec velit luctus, vitae scelerisque augue imperdiet. Nam sit amet dolor at odio congue interdum. Cras malesuada quam vitae porttitor fringilla. Nullam nec nisl arcu. Curabitur pharetra nisi ut urna vehicula, faucibus posuere dolor rhoncus. Vivamus scelerisque dolor sapien, et facilisis justo rhoncus a. Vestibulum et nisi et ante tempor dapibus. Fusce ut enim at ex elementum feugiat sed eu risus. Curabitur consequat, est nec feugiat maximus, nisl leo condimentum enim, sit amet scelerisque nibh dolor et dui. Nam eget semper purus. Nam erat ipsum, lobortis at accumsan id, euismod a purus.
+                        </p>
                     </div>
                 </div>
             </section>
